c1d053f8e6dc96018e40110afa6ca4e479a129da
Elly <diallops01@gmail.com>
2021-10-19 17:37:23 +0200

    password-validator

---
+++ middleware/password.js
@@ -0,0 +1,34 @@
+const passwordValidator = require("password-validator");
+
+
+const passwordSchema  = new passwordValidator();  
+
+
+passwordSchema 
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase(1)                              // Must have uppercase letters
+.has().lowercase(1)                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123','azerty']); // Blacklist these values
+
+
+
+module.exports = (req, res, next) => {
+
+    if (passwordSchema.validate(req.body.password)) {
+
+        next();
+
+    } else {
+
+        return res
+        .status(400)
+        .json({Error : "le mot de passe n'est pas assez fort" + alert(passwordSchema.validate(req.body.password, {list: true}))})
+        
+    }
+
+
+
+}
